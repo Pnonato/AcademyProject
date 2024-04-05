89b5cb9c71b85ee7f2da707a12d83142b5fc97b4
Liz <141193412+LizzBricce@users.noreply.github.com>
2024-04-05 13:43:07 -0300

    Add files via upload

---
+++ DEOO_Gym/src/main/java/br/com/deoo/gym/deoo_gym/DeooGymApplication.java
@@ -0,0 +1,13 @@
+package br.com.deoo.gym.deoo_gym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeooGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeooGymApplication.class, args);
+	}
+
+}
---
+++ DEOO_Gym/src/test/java/br/com/deoo/gym/deoo_gym/DeooGymApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.deoo.gym.deoo_gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeooGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

3ff960193bab1a09eb1849c5b99acb007bae4f81
Giovani Oliveira <giovani.oliveiracpv@gmail.com>
2024-04-09 10:44:22 -0300

    Criação das outras camadas

---
+++ DEOO_Gym/src/main/java/br/com/deoo/gym/deoo_gym/useCases/ImcCalculation.java
@@ -0,0 +1,12 @@
+package br.com.deoo.gym.deoo_gym.useCases;
+
+public interface ImcCalculation {
+
+
+
+
+
+    public static void imcCalculation(){
+
+    }
+}
---
+++ DEOO_Gym/src/main/java/br/com/deoo/gym/deoo_gym/useCases/LoginManegment.java
@@ -0,0 +1,4 @@
+package br.com.deoo.gym.deoo_gym.useCases;
+
+public interface LoginManegment {
+}

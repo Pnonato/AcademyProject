dcaf7663226d2bb8696264861142a3330929b612
Giovani Oliveira <giovani.oliveiracpv@gmail.com>
2024-04-11 14:44:57 -0300

    TrainingDAO

---
+++ DEOO_Gym/src/main/java/br/com/deoo/gym/deoo_gym/frameworksAndDrivers/dao/TrainingDAO.java
@@ -0,0 +1,15 @@
+package br.com.deoo.gym.deoo_gym.frameworksAndDrivers.dao;
+
+public interface TrainingDAO {
+
+    String getAll();
+    String listExerciseA();
+    String listExerciseB();
+    String listExerciseC();
+    void editExercise();
+    void deleteExercise();
+
+
+
+
+}

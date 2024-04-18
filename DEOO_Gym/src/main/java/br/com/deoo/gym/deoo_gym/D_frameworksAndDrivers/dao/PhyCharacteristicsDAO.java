464ed345d47d8f3538b6bf8c2c9b7486e4ef89dc
Pnonato <like.nonato.marrara@gmail.com>
2024-04-18 17:15:21 -0300

    PhyCharacteristicsDao implementados

--- DEOO_Gym/src/main/java/br/com/deoo/gym/deoo_gym/D_frameworksAndDrivers/dao/PhyCharacteristicsDAO.java
+++ DEOO_Gym/src/main/java/br/com/deoo/gym/deoo_gym/D_frameworksAndDrivers/dao/PhyCharacteristicsDAO.java
@@ -1,8 +1,15 @@
 package br.com.deoo.gym.deoo_gym.D_frameworksAndDrivers.dao;
 
+import br.com.deoo.gym.deoo_gym.A_entity.PhysicalCharacteristics;
+
+import java.util.List;
+
 public interface PhyCharacteristicsDAO {
 
 
-
+    List<PhysicalCharacteristics> getAll();
+    void addCharacteristics(PhysicalCharacteristics characteristics);
+    void updateCharacteristics(PhysicalCharacteristics updatedCharacteristics);
+    void deleteCharacteristics(String gender);
 
 }
---
+++ DEOO_Gym/src/main/java/br/com/deoo/gym/deoo_gym/D_frameworksAndDrivers/dao/PhyCharacteristicsDaoIMP.java
@@ -0,0 +1,39 @@
+package br.com.deoo.gym.deoo_gym.D_frameworksAndDrivers.dao;
+
+import br.com.deoo.gym.deoo_gym.A_entity.PhysicalCharacteristics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PhyCharacteristicsDaoIMP {
+    private List<PhysicalCharacteristics> characteristicsList;
+
+    public PhyCharacteristicsDaoIMP() {
+        this.characteristicsList = new ArrayList<>();
+    }
+
+    public List<PhysicalCharacteristics> getAll() {
+        return characteristicsList;
+    }
+
+    public void addCharacteristics(PhysicalCharacteristics characteristics) {
+        characteristicsList.add(characteristics);
+    }
+
+    public void updateCharacteristics(PhysicalCharacteristics updatedCharacteristics) {
+        for (int i = 0; i < characteristicsList.size(); i++) {
+            PhysicalCharacteristics characteristics = characteristicsList.get(i);
+            if (characteristics.getGender().equals(updatedCharacteristics.getGender())) {
+                // Atualizar as características físicas com base no gênero (pode ser necessário adicionar mais critérios de comparação)
+                characteristicsList.set(i, updatedCharacteristics);
+                return;
+            }
+        }
+
+    }
+
+    public void deleteCharacteristics(String gender) {
+        characteristicsList.removeIf(characteristics -> characteristics.getGender().equals(gender));
+
+    }
+}
